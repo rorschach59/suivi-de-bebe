6c5248f749298af53004b9fc4b679b8477418e87
Rémi <contact@remipouly.fr>
2023-08-13 12:47:51 +0200

    suppression bouton ouverture modal

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import './bootstrap';
 import 'flowbite';
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
+import {Modal} from 'flowbite';
 
 import.meta.glob([
     '../images/**'
@@ -17,7 +18,24 @@ window.Echo = new Echo({
 });
 
 let channel = window.Echo.channel(import.meta.env.VITE_PUSHER_CHANNEL);
-channel.listen('.refresh-application', function(data) {
+channel.listen('.refresh-application', function (data) {
     console.log(JSON.stringify(data))
     Livewire.emit('refreshApplication');
 });
+
+const updateReminderModal = document.getElementById('updateReminderModal');
+const modal = new Modal(updateReminderModal);
+const reminder = document.getElementById('reminderTextarea');
+const closeModal = document.getElementById('closeModal');
+
+if(reminder !== null) {
+    reminder.addEventListener("click", event => {
+        modal.show();
+    })
+}
+
+if(closeModal !== null) {
+    closeModal.addEventListener("click", event => {
+        modal.hide();
+    })
+}
--- resources/views/livewire/view/day.blade.php
+++ resources/views/livewire/view/day.blade.php
@@ -75,7 +75,7 @@
         <h2 id="reminder" class="font-bold text-center">Pense-bête</h2>
         <div class="justify-self-end self-end pr-2">
 
-            <button data-modal-target="updateReminderModal" data-modal-toggle="updateReminderModal" class=""
+            <button data-modal-target="updateReminderModal" data-modal-toggle="updateReminderModal" class="hidden"
                     type="button">
                 <img src="{{ Vite::asset('resources/images/stylo.svg') }}" alt="Stylo">
             </button>
@@ -90,17 +90,6 @@
                             <h3 class="font-semibold text-gray-900">
                                 Modifier le pense-bête
                             </h3>
-                            <button type="button"
-                                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center"
-                                    data-modal-hide="updateReminderModal">
-                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                     viewBox="0 0 14 14">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                          stroke-width="2"
-                                          d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                                </svg>
-                                <span class="sr-only">Close modal</span>
-                            </button>
                         </div>
                         <form action="{{ route('post.reminder') }}#reminder" method="post" class="bg-myGreen-200">
                             @csrf
@@ -113,10 +102,10 @@
                         >{!! $reminder !!}</textarea>
                             </div>
                             <div class="flex justify-around p-2 space-x-2">
-                                <button data-modal-hide="updateReminderModal" type="submit" class="primary-button">
+                                <button type="submit" class="primary-button">
                                     Modifier
                                 </button>
-                                <button data-modal-hide="updateReminderModal" type="button" class="secondary-button">
+                                <button id="closeModal" type="button" class="secondary-button">
                                     Annuler
                                 </button>
                             </div>
@@ -128,7 +117,7 @@
         </div>
     </div>
 
-    <div class="p-2 mt-2 bg-myGreen-400 rounded-lg mb-14 {{ empty($reminder) ? 'h-10' : '' }}">
+    <div id="reminderTextarea" class="p-2 mt-2 bg-myGreen-400 rounded-lg mb-14 {{ empty($reminder) ? 'h-10' : '' }}">
         @if( !empty($reminder) )
             {!! nl2br($reminder) !!}
         @endif
